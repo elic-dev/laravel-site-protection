3ff91ead19d34d20d3753e497e1961b1258a6852
kurumbus <grigory.shein@gmail.com>
2019-09-15 15:47:46 +0300

    add hints

--- src/config/site-protection.php
+++ src/config/site-protection.php
@@ -13,6 +13,6 @@ return [
     */
 
     'passwords' => env('SITE_PROTECTION_PASSWORDS'),
-    'except_paths' => env('SITE_PROTECTION_EXCEPT_PATHS')
+    'except_paths' => env('SITE_PROTECTION_EXCEPT_PATHS') // comma-separated, home as "/", e.g. "/,register,some-url"
 
 ];
